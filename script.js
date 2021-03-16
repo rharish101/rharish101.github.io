b0ab65a6219f7adcecdd457e6baf95d83cef497b
Harish Rajagopal <harish.rajagopals@gmail.com>
2021-03-16 20:05:45 +0100

    Mode indicator now has different icons per mode

--- script.js
+++ script.js
@@ -19,11 +19,15 @@ function setMode()
   }
 
   if (mode != "dark")
+  {
+    document.getElementById("toggle-mode").name = "moon";
     return;
+  }
 
   // Disable transitions temporarily, as they lead to a white flash
   document.body.style.transitionDuration = "0s";
   document.body.classList.toggle("dark-mode");
+  document.getElementById("toggle-mode").name = "sunny";
   setTimeout(
     function () {
       document.body.style.transitionDuration = "0.1s";
@@ -35,9 +39,15 @@ function toggleMode()
 {
   document.body.classList.toggle("dark-mode");
   if (document.body.classList.contains("dark-mode"))
+  {
     window.localStorage.setItem("mode", "dark");
+    document.getElementById("toggle-mode").name = "sunny";
+  }
   else
+  {
     window.localStorage.setItem("mode", "light");
+    document.getElementById("toggle-mode").name = "moon";
+  }
 }
 
 setMode();
