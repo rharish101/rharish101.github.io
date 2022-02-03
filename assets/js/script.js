3f777236efdc2a3014e281ccefe1df674d1b0038
Harish Rajagopal <harish.rajagopals@gmail.com>
2022-02-03 09:54:52 +0100

    Simplified constants to use maps instead of separate variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,25 @@
 const MODE_BTN_ID = "mode-btn";
 const MODE_ICON_ID = "mode-icon";
 
-// Icon for light/dark mode switching
-const LIGHT_MODE_ICON = "bi-sun-fill"; // shown in dark mode
-const DARK_MODE_ICON = "bi-moon-fill"; // shown in light mode
-
-// Text shown on hovering
-const LIGHT_MODE_TEXT = "Switch to light mode"; // shown in dark mode
-const DARK_MODE_TEXT = "Switch to dark mode"; // shown in light mode
-
-// Transition duration for the background
-const TRANSITION = "0.1s";
-
 // Root node attribute for the mode/theme and its values
 const MODE_ATTR = "data-theme";
 const LIGHT_MODE_VAL = "light";
 const DARK_MODE_VAL = "dark";
+
+// Icon for light/dark mode switching
+const MODE_ICON = new Map([
+  [LIGHT_MODE_VAL, "bi-sun-fill"], // shown in dark mode
+  [DARK_MODE_VAL, "bi-moon-fill"], // shown in light mode
+]);
+
+// Text shown on hovering
+const MODE_TEXT = new Map([
+  [LIGHT_MODE_VAL, "Switch to light mode"], // shown in dark mode
+  [DARK_MODE_VAL, "Switch to dark mode"], // shown in light mode
+]);
+
+// Transition duration for the background
+const TRANSITION = "0.1s";
 
 function getPreferredMode()
 {
@@ -41,7 +45,7 @@ function initMode()
 
   if (mode != DARK_MODE_VAL)
   {
-    document.getElementById(MODE_ICON_ID).classList = DARK_MODE_ICON;
+    document.getElementById(MODE_ICON_ID).classList = MODE_ICON.get(preferredMode);
     return;
   }
 
@@ -61,21 +65,23 @@ function toggleMode()
   var modeButton = document.getElementById(MODE_BTN_ID);
   var modeIcon = document.getElementById(MODE_ICON_ID);
 
+  var newMode;
   switch (document.documentElement.getAttribute(MODE_ATTR))
   {
     case DARK_MODE_VAL:
-      rootNode.setAttribute(MODE_ATTR, LIGHT_MODE_VAL);
-      modeIcon.classList = DARK_MODE_ICON;
-      modeButton.title = DARK_MODE_TEXT;
-      return LIGHT_MODE_VAL;
+      newMode = LIGHT_MODE_VAL;
+      break;
 
     case LIGHT_MODE_VAL:
     default:
-      rootNode.setAttribute(MODE_ATTR, DARK_MODE_VAL);
-      modeIcon.classList = LIGHT_MODE_ICON;
-      modeButton.title = LIGHT_MODE_TEXT;
-      return DARK_MODE_VAL;
+      newMode = DARK_MODE_VAL;
+      break;
   }
+
+  rootNode.setAttribute(MODE_ATTR, newMode);
+  modeIcon.classList = MODE_ICON.get(newMode);
+  modeButton.title = MODE_TEXT.get(newMode);
+  return newMode;
 }
 
 function toggleAndSetMode()
