63f4b3d3de8b5388b210a712a5ecf7f0770ac30e
Harish Rajagopal <harish.rajagopals@gmail.com>
2022-12-06 18:16:46 +0100

    Light/dark mode toggle now has a "default" mode
    Default mode defers to the browser's preferred mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,15 @@ const MODE_ICON_ID = "mode-icon";
 
 // Root node attribute for the mode/theme and its values
 const MODE_ATTR = "data-theme";
+const DEFAULT_MODE_VAL = "default";
 const LIGHT_MODE_VAL = "light";
 const DARK_MODE_VAL = "dark";
 
 // Icon for light/dark mode switching
 const MODE_ICON = new Map([
-  [DARK_MODE_VAL, "bi-sun-fill"], // shown in dark mode
-  [LIGHT_MODE_VAL, "bi-moon-fill"], // shown in light mode
-]);
-
-// Text shown on hovering
-const MODE_TEXT = new Map([
-  [DARK_MODE_VAL, "Switch to light mode"], // shown in dark mode
-  [LIGHT_MODE_VAL, "Switch to dark mode"], // shown in light mode
+  [DEFAULT_MODE_VAL, "bi-circle-half"], // shown by default
+  [DARK_MODE_VAL, "bi-moon-fill"], // shown in dark mode
+  [LIGHT_MODE_VAL, "bi-sun-fill"], // shown in light mode
 ]);
 
 // Transition duration for the background
@@ -39,13 +35,12 @@ function initMode()
   var modeButton = document.getElementById(MODE_BTN_ID);
   modeButton.style.display = "block";
 
-  var mode = window.localStorage.getItem("mode");
+  var mode = window.localStorage.getItem("mode") || DEFAULT_MODE_VAL;
   var preferredMode = getPreferredMode();
   // SASS sets the default styling to the browser preferred mode
-  if (!mode || mode == preferredMode)
+  if (mode == DEFAULT_MODE_VAL || mode == preferredMode)
   {
-    document.getElementById(MODE_BTN_ID).title = MODE_TEXT.get(preferredMode);
-    document.getElementById(MODE_ICON_ID).classList = MODE_ICON.get(preferredMode);
+    document.getElementById(MODE_ICON_ID).classList = MODE_ICON.get(mode);
     return;
   }
 
@@ -62,28 +57,27 @@ function initMode()
 function toggleMode()
 {
   var rootNode = document.documentElement;
-  var modeButton = document.getElementById(MODE_BTN_ID);
   var modeIcon = document.getElementById(MODE_ICON_ID);
-
-  var currMode = document.documentElement.getAttribute(MODE_ATTR);
-  if (!currMode)
-    currMode = getPreferredMode();
+  var currMode = document.documentElement.getAttribute(MODE_ATTR) || DEFAULT_MODE_VAL;
 
   var newMode;
   switch (currMode)
   {
     case DARK_MODE_VAL:
-      newMode = LIGHT_MODE_VAL;
+      newMode = DEFAULT_MODE_VAL;
       break;
 
     case LIGHT_MODE_VAL:
       newMode = DARK_MODE_VAL;
       break;
+
+    case DEFAULT_MODE_VAL:
+      newMode = LIGHT_MODE_VAL;
+      break;
   }
 
   rootNode.setAttribute(MODE_ATTR, newMode);
   modeIcon.classList = MODE_ICON.get(newMode);
-  modeButton.title = MODE_TEXT.get(newMode);
   return newMode;
 }
 
